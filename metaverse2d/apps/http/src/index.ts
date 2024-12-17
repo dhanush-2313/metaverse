adab39d2f25d942fb62fabaa0706954c01f41fb8
Dhanush <81028546+dhanush-2313@users.noreply.github.com>
2024-12-17 09:47:00 +0530

    fixed import outside module added a build script

--- metaverse2d/apps/http/src/index.ts
+++ metaverse2d/apps/http/src/index.ts
@@ -6,5 +6,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/v1",router);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("Server is running");
+});
 
--- metaverse2d/apps/http/src/routes/v1/index.ts
+++ metaverse2d/apps/http/src/routes/v1/index.ts
@@ -1,92 +1,92 @@
-import { Router} from "express";
+import { Router } from "express";
 import { userRouter } from "./user";
 import { spaceRouter } from "./space";
 import { adminRouter } from "./admin";
 import { SigninSchema, SignupSchema } from "../../types";
 import bcrypt from "bcryptjs";
-import client from "@repo/db/client";
+import client from "@repo/db/client"
 import jwt from "jsonwebtoken";
 import { JWT_PASSWORD } from "../../config";
 
 export const router = Router();
 
 
-router.post("/signup",async(req,res)=>{
+router.post("/signup", async (req, res) => {
     const parsedData = SignupSchema.safeParse(req.body);
-    if(!parsedData.success){
+    if (!parsedData.success) {
         res.status(400).json({
-            message:"Invalid data"
+            message: "Invalid data"
         })
         return;
     }
 
-    const hashedPassword = await bcrypt.hash(parsedData.data.password,10);
+    const hashedPassword = await bcrypt.hash(parsedData.data.password, 10);
 
     try {
         const user = await client.user.create({
-            data:{
-                username:parsedData.data.username,
-                password:hashedPassword,
-                role: parsedData.data.type ==="admin"?"Admin" : "User"
+            data: {
+                username: parsedData.data.username,
+                password: hashedPassword,
+                role: parsedData.data.type === "admin" ? "Admin" : "User"
             }
         })
         res.status(200).json({
-            userId : user.id
+            userId: user.id
         })
-        
+
     } catch (error) {
-        res.status(400).json({message:"User already exists"})
+        res.status(400).json({ message: "User already exists" })
     }
-  
+
 })
 
-router.post("/signin",async(req,res)=>{
+router.post("/signin", async (req, res) => {
     const parsedData = SigninSchema.safeParse(req.body);
-    if(!parsedData.success){
+    if (!parsedData.success) {
         res.status(400).json({
-            message:"Invalid data"
+            message: "Invalid data"
         })
         return;
     }
 
     try {
         const user = await client.user.findUnique({
-            where:{
-                username:parsedData.data.username
+            where: {
+                username: parsedData.data.username
             }
         })
-        if(!user){
-            res.status(403).json({message:"User not found"})
+        if (!user) {
+            res.status(403).json({ message: "User not found" })
             return;
         }
-        const isValid = await bcrypt.compare(parsedData.data.password,user.password);
-        if(!isValid){
-            res.status(403).json({message:"Invalid credentials"})
+        const isValid = await bcrypt.compare(parsedData.data.password, user.password);
+        if (!isValid) {
+            res.status(403).json({ message: "Invalid credentials" })
             return;
         }
 
-        const token = jwt.sign({userId:user.id,role:user.role},JWT_PASSWORD);
+        const token = jwt.sign({ userId: user.id, role: user.role }, JWT_PASSWORD);
         res.status(200).json({
             token
         })
     } catch (error) {
-        res.status(400).json({message:"Invalid credentials"})
+        res.status(400).json({ message: "Invalid credentials" })
     }
 
-    
-})
-
-router.get("elements",(req,res)=>{
 
 })
 
-router.get("avatars",(req,res)=>{
-    
+router.get("elements", (req, res) => {
+
 })
 
-router.use("/user",userRouter);
-router.use("/space",spaceRouter);
-router.use("/admin",adminRouter);
+router.get("avatars", (req, res) => {
+
+})
+
+router.use("/user", userRouter);
+router.use("/space", spaceRouter);
+router.use("/admin", adminRouter);
 
 
 
