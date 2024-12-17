b608968e631e7d0ce03b26ca0f7679a96c459925
Dhanush <81028546+dhanush-2313@users.noreply.github.com>
2024-12-17 23:28:39 +0530

    middlwares

--- metaverse2d/apps/http/src/routes/v1/user.ts
+++ metaverse2d/apps/http/src/routes/v1/user.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
+import { UpdateMetadataSchema } from "../../types";
 
 export const userRouter = Router();
 
 userRouter.post("/metadata",(req,res)=>{
+    const parsedData = UpdateMetadataSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.status(400).json({
+            message:"Invalid data"
+        })
+        return;
+    }
 
 })
 
 userRouter.get("/metadata/bulk",(req,res)=>{
-
 })
