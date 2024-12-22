30a89a2bcec6551dc4dc484ea3c7703bb9d5e47d
Dhanush <81028546+dhanush-2313@users.noreply.github.com>
2024-12-22 22:36:04 +0530

    some change

--- metaverse2d/apps/http/src/routes/v1/space.ts
+++ metaverse2d/apps/http/src/routes/v1/space.ts
@@ -1,9 +1,28 @@
 import { Router } from "express";
+import client from "@repo/db/client";
+import { CreateSpaceSchema } from "../../types";
 
 export const spaceRouter = Router();
 
-spaceRouter.post("/",(req,res)=>{
+spaceRouter.post("/",async(req,res)=>{
+    const parsedData = CreateSpaceSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.status(400).json({
+            message:"Validation error"
+        })
+        return;
+    }
 
+    if(!parsedData.data.mapId){
+        await client.space.create({
+            data:{
+                name:parsedData.data.name,
+                width:Number(parsedData.data.dimensions.split("x")[0]),
+                height:Number(parsedData.data.dimensions.split("x")[1]),
+                creatorId:req.userId
+            }
+        })
+    }
 })
 
 spaceRouter.delete("/:spaceId",(req,res)=>{
